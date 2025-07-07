a15faa34be769b94e27f5e4ab32fa49f91bd5f42
Henrique Hoinacki <henrique.hoinacki@ilia.digital>
2025-07-06 23:01:41 -0300

    ⬇️ Ajust domain

--- src/components/pages/Perfil.tsx
+++ src/components/pages/Perfil.tsx
@@ -61,7 +61,7 @@ const Perfil = () => {
               {personalInfo.phone}
             </p>
             <a
-              href="/perfil-henrique-hoinacki/Henrique_Hoinacki_CV.pdf"
+              href="/Henrique_Hoinacki_CV.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block bg-gray-700 hover:bg-gray-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200"
